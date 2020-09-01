e76e93622f0fca0480e2a77039cf2649ae657ba3
TRose2000 <teamddg@hotmail.com>
2020-09-01 10:21:07 +0200

    test

---
+++ app.js
@@ -0,0 +1,10 @@
+{
+        "port": 25500,
+        "secret": "secretHash",
+        "projects": [
+                {
+                        "name": "Wumpus-Universe/test",
+                        "directory": "/home/ullabritta/Thrasilias/testing"
+                }
+        ]
+}
